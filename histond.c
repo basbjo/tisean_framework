d3e992032b693d67b457c3d23df9423c950e9c81
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-07-07 13:33:00 +0200

    histond: Allow for arbitrary number of columns (1)

--- histond.c
+++ histond.c
@@ -74,11 +74,11 @@ void scan_options(int n,char **argv)
 int main(int argc,char **argv)
 {
   char stdi=0;
-  double base_1,sx,sy,norm2;
-  double min[2],interval[2];
+  double base_1,norm2;
+  double min[dim],interval[dim];
   double **series;
   unsigned long i,j;
-  unsigned int bi,bj;
+  unsigned int n,bi[dim];
   unsigned long **box;
   FILE *fout=NULL;
 
@@ -113,20 +113,17 @@ int main(int argc,char **argv)
     series=(double**)get_multi_series(infile,&length,exclude,&dim,columns,
                                       dimset,verbosity);
 
-  min[0]=interval[0]=series[0][0];
-  min[1]=interval[1]=series[1][0];
-  for (i=1;i<length;i++) {
-    if (series[0][i] < min[0]) min[0]=series[0][i];
-    else if (series[0][i] > interval[0]) interval[0]=series[0][i];
-    if (series[1][i] < min[1]) min[1]=series[1][i];
-    else if (series[1][i] > interval[1]) interval[1]=series[1][i];
-  }
-  interval[0] -= min[0];
-  interval[1] -= min[1];
+  for (n=0;n<dim;n++) {
+    min[n]=interval[n]=series[n][0];
+    for (i=1;i<length;i++) {
+      if (series[n][i] < min[n]) min[n]=series[n][i];
+      else if (series[n][i] > interval[n]) interval[n]=series[n][i];
+    }
+    interval[n] -= min[n];
 
-  for (i=0;i<length;i++) {
-    series[0][i]=(series[0][i]-min[0]);
-    series[1][i]=(series[1][i]-min[1]);
+    for (i=0;i<length;i++) {
+      series[n][i]=(series[n][i]-min[n]);
+    }
   }
 
   check_alloc(box=(unsigned long**)malloc(sizeof(unsigned long*)*base));
@@ -136,16 +133,17 @@ int main(int argc,char **argv)
       box[i][j]=1;
   }
   base_1=(double)base;
-  sx=interval[0]/base_1;
-  sy=interval[1]/base_1;
-  norm2=(double)(length+base*base)*sx*sy;
+  norm2=(double)(length+pow(base,dim));
+  for (n=0;n<dim;n++) {
+    norm2*=(double)interval[n]/base_1;
+  }
 
   for (i=0;i<length;i++) {
-    bi=(unsigned int)(series[0][i]*base_1/interval[0]);
-    bj=(unsigned int)(series[1][i]*base_1/interval[1]);
-    bi=(bi>=base)? base-1:bi;
-    bj=(bj>=base)? base-1:bj;
-    box[bi][bj]++;
+    for (n=0;n<dim;n++) {
+      bi[n]=(unsigned int)(series[n][i]*base_1/interval[n]);
+      bi[n]=(bi[n]>=base)? base-1:bi[n];
+    }
+    box[bi[0]][bi[1]]++;
   }
 
   if (!stout)
@@ -153,19 +151,22 @@ int main(int argc,char **argv)
 
   fout=fopen(outfile,"w");
 
-  interval[0] /= base_1;
-  interval[1] /= base_1;
+  for (n=0;n<dim;n++) {
+    interval[n] /= base_1;
+  }
   for (i=0;i<base;i++) {
     for (j=0;j<base;j++) {
       if (stout) {
-        fprintf(stdout,"%e %e %e\n",((double)(i)+0.5)*interval[0]+min[0],
-                ((double)(j)+0.5)*interval[1]+min[1],
-                (double)box[i][j]/norm2);
+        fprintf(stdout,"%e %e ",
+                ((double)(i)+0.5)*interval[0]+min[0],
+                ((double)(j)+0.5)*interval[1]+min[1]);
+        fprintf(stdout,"%e\n",(double)box[i][j]/norm2);
       }
       else {
-        fprintf(fout,"%e %e %e\n",((double)(i)+0.5)*interval[0]+min[0],
-                ((double)(j)+0.5)*interval[1]+min[1],
-                (double)box[i][j]/norm2);
+        fprintf(fout,"%e %e ",
+                ((double)(i)+0.5)*interval[0]+min[0],
+                ((double)(j)+0.5)*interval[1]+min[1]);
+        fprintf(fout,"%e\n",(double)box[i][j]/norm2);
       }
     }
     if (stout)
@@ -183,8 +184,9 @@ int main(int argc,char **argv)
   for (i=0;i<base;i++)
     free(box[i]);
   free(box);
-  free(series[0]);
-  free(series[1]);
+  for (n=0;n<dim;n++) {
+    free(series[n]);
+  }
   free(series);
 
   return 0;
